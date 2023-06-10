cc46e3a8cde76c066b2ceec861c16143f7de51da
lineno2 <hosun1804@gmail.com>
2023-06-10 18:49:27 +0900

    [ADD] work record deletion

--- work_manage_add.php
+++ work_manage_add.php
@@ -10,9 +10,6 @@ $result = mysqli_query($conn, $sql);
 if($result){
     echo "<script>alert('근무 추가 완료');history.back();</script>";
 }else{
-    echo "<script>alert('".$work_date."');history.back();</script>";
-    echo $work_date;
-    echo $start_time;
-    echo $end_time;
+    echo "<script>alert('근무 추가 실패');history.back();</script>";
 }
 ?>
