9d3551b1a7bff9c7459e19936486a7e372311e4f
lineno2 <hosun1804@gmail.com>
2023-06-10 17:35:01 +0900

    [MOD] url path

--- employee_search.php
+++ employee_search.php
@@ -4,7 +4,7 @@ include 'header.php';
 ?>
     <h1>직원 검색</h1>
     <p>근무시간 관리할 직원을 검색하세요</p>
-    <form action="employee_search_process.php" method="POST">
+    <form action="employee_search_result.php" method="POST">
   <div class="mb-3">
     <label for="employee_info" class="form-label">직원 정보</label>
     <input type="text" class="form-control" id="employee_info" name="employee_info" placeholder="직원 이름, 전화번호">
