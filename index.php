07762d650223574f86e5c8bf644b90192f40c6ed
lineno2 <hosun1804@gmail.com>
2023-06-10 14:42:50 +0900

    [FIX] db connection

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php 
 $title = "메인페이지";
 include 'header.php'; 
+include 'db.php';
 ?>
 <body>
     <h1>아니 php야 좀 실행을 해봐</h1>
     <?php
     $sql = "SELECT * FROM `employee`";
-    // $result = mysqli_query($conn, $sql);
     echo '왜 안나와요?';
     echo $sql;
-    // echo $result->num_rows;
+    $result = mysqli_query($conn, $sql);
+    echo $result->num_rows;
     ?>  
 </body>
 </html>
