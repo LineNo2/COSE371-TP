fe1fa675066628f0c0fc7a96726476dfc813f6ec
lineno2 <hosun1804@gmail.com>
2023-06-10 21:10:37 +0900

    [MOD] search query debug

--- employee_search_ajax.php
+++ employee_search_ajax.php
@@ -2,7 +2,8 @@
 $keyword = $_GET['keyword'];
 $sql = "SELECT * FROM employee WHERE name LIKE '%$keyword%' OR emp_tel LIKE '%$keyword%'";
 $result = mysqli_query($conn, $sql);
-echo 'employee = [';
+echo $sql;
+echo ';employee = [';
 while($row = mysqli_fetch_array($result)){
     echo '{';
     echo 'emp_no: '.$row['emp_no'].',';
