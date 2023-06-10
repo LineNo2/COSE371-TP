6adf53220b4a8c3db4b0bad47a0defc38d1cef17
lineno2 <hosun1804@gmail.com>
2023-06-10 14:37:01 +0900

    [FIX] db connection

--- header.php
+++ header.php
@@ -10,4 +10,3 @@
      <title><?php echo $title; ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
  </head>
-<body>
--- index.php
+++ index.php
@@ -3,6 +3,7 @@ $title = "메인페이지";
 include 'header.php'; 
 ?>
 <body>
+    <h1>아니 php야 좀 실행을 해봐</h1>
     <?php
     $sql = "SELECT * FROM `employee`";
     $result = mysqli_query($conn, $sql);
