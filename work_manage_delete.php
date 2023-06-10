4b6e4c6a5b92ea11d5ebc80e4856a42b87f83035
lineno2 <hosun1804@gmail.com>
2023-06-10 19:06:27 +0900

    [FIX] Delete query

--- work_manage_delete.php
+++ work_manage_delete.php
@@ -3,12 +3,12 @@ include 'db.php';
 $emp_no = $_GET['emp_no'];
 $work_begin = $_GET['work_begin'];
 $work_end = $_GET['work_end'];
-$sql = "DELETE FROM work_record WHERE emp_no = $emp_no AND work_begin = '$work_begin' AND work_end = '$work_end'";
+$sql = "DELETE FROM work_record WHERE emp_no = $emp_no and work_begin = '$work_begin' and work_end = '$work_end'";
 echo $sql;
 $result = mysqli_query($conn, $sql);
 if($result){
     echo "<script>alert('근무 삭제 완료');history.back();</script>";
 }else{
-    echo "<script>alert('근무 삭제 실패');history.back();</script>";
+    echo "<script>alert('$sql');history.back();</script>";
 }
 ?>
